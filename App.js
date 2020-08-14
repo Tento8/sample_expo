bc7af01a1fa916f592850d9315782bea051205dc
八幡亮生 <yawataryosei@rysnro.local>
2020-08-14 13:34:54 +0900

    first expo!

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>屋台</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>抽選</Text>
+    </View>
+  );
+}
+
+function RulesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>ルール</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="屋台" component={HomeScreen} />
+          <Tab.Screen name="抽選" component={SettingsScreen} />
+          <Tab.Screen name="ルール" component={RulesScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
 
